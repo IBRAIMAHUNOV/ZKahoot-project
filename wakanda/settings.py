a7e80e19e8e90d1e9ab67373d271fceeebb1dc0e
Arstan <58651451+IBRAIMAHUNOV@users.noreply.github.com>
2022-08-11 21:05:26 +0600

    Update settings.py

--- wakanda/settings.py
+++ wakanda/settings.py
@@ -171,7 +171,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 AUTH_USER_MODEL = 'fastest.UserModels'
 AUTH_USER_MODELL = 'fastest.CustomUserManager'
